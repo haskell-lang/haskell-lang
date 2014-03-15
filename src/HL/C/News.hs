5f1b05e755436cbf29a0816ff43bc4d8fe5b9f25
Chris Done <chrisdone@gmail.com>
2014-03-15 01:56:55 +0100

    Small comment for news module

--- src/HL/C/News.hs
+++ src/HL/C/News.hs
@@ -1,4 +1,6 @@
--- | News page controller.
+-- | News page controller. This page simply downloads from
+-- haskellnews.org which already has a pre-prepared page of news to
+-- display.
 
 module HL.C.News where
 
