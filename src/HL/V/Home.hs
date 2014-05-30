e60abb4560a824b2c10c091a08f057ef8d68c942
Chris Done <chrisdone@gmail.com>
2014-05-31 01:50:44 +0200

    Add some starter content for homepage

--- src/HL/V/Home.hs
+++ src/HL/V/Home.hs
@@ -107,34 +107,73 @@ features =
   div [class_ "features"]
       (container
          (do h1 [] "Features"
-             row (do span6 []
-                           (do h2 [] "Purely functional"
-                               p [] lorem
-                               p [] (a [] "View examples"))
-                     span6 []
-                           (do h2 [] "Statically typed"
-                               p [] lorem
-                               p [] (a [] "View examples")))
-             row (do span6 []
-                           (do h2 [] "Concurrent"
-                               p [] lorem
-                               p [] (a [] "View examples"))
-                     span6 []
-                           (do h2 [] "Type inference"
-                               p [] lorem
-                               p [] (a [] "View examples")))
-             row (do span6 []
-                           (do h2 [] "Lazy"
-                               p [] lorem
-                               p [] (a [] "View examples"))
-                     span6 []
-                           (do h2 [] "Packages"
-                               p [] lorem
-                               p [] (a [] "View examples")))))
-  where
-    lorem =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra nisl non elit consectetur sodales. Ut condimentum odio in augue scelerisque, eget ultricies arcu placerat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris a blandit purus, vitae tincidunt leo. "
+             row (do span6 [] purefunc
+                     span6 [] statically)
+             row (do span6 [] concurrent
+                     span6 [] inference)
+             row (do span6 [] lazy
+                     span6 [] packages)))
+
+-- Note: these below are me writing out the facts, for myself, rather
+-- than putting in a way that newbies will understand. The intention
+-- is to put *something* here and then rewrite the bits below to be
+-- more "salesy" aand friendly to people who have no idea what's so
+-- special about "IO" from any other form of programming, or what a
+-- parallel GC is or unification.
+
+purefunc =
+  do h2 [] "Purely functional"
+     p [] "Every function in Haskell is pure. They are functions in the mathematical sense. \
+          \Even side-effecting IO operations are but a description of what to do, produced \
+          \by pure code. There are no statements or instructions, only expressions. Which \
+          \cannot mutate variables, local or global, or access state like time or random \
+          \numbers."
+     p [] (a [] "View examples")
+
+statically =
+  do h2 [] "Statically typed"
+     p [] "Every expression in Haskell has a type which is determined at compile time. \
+          \All the types composed together by function application have to match up. If \
+          \they don't, the program will be rejected by the compiler. Types become not \
+          \only a form of guarantee, but a language for expressing the construction \
+          \of programs."
+     p [] (a [] "View examples")
+
+concurrent =
+  do h2 [] "Concurrent"
+     p [] "Haskell lends itself well to concurrent programming due to its explicit \
+          \handling of effects. Its flagship compiler, GHC, comes with a high-\
+          \performance parallel garbage collector and light-weight concurrency \
+          \library containing a number of useful concurrency primitives and \
+          \abstractions."
+     p [] (a [] "View examples")
+
+inference =
+  do h2 [] "Type inference"
+     p [] "You don't have to explicitly write out every type in a Haskell program. \
+          \Types will be inferred by unifying every type bidirectionally. But you \
+          \can write out types, or ask the compiler to write them for you, for \
+          \handy documentation."
+     p [] (a [] "View examples")
+
+lazy =
+  do h2 [] "Lazy"
+     p [] "Functions don't evaluate their arguments. This means that programs \
+          \can compose together very well, with the ability to write control \
+          \constructs with normal functions, and, thanks also to the purity \
+          \of Haskell, to fuse chains of functions together for high \
+          \performance."
+     p [] (a [] "View examples")
+
+packages =
+  do h2 [] "Packages"
+     p [] "Open source contribution to Haskell is very active with a wide range \
+          \of packages available on the public package servers."
+     p [] (a [] "View examples")
 
 events :: Senza
 events =
   return ()
+
+lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra nisl non elit consectetur sodales. Ut condimentum odio in augue scelerisque, eget ultricies arcu placerat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris a blandit purus, vitae tincidunt leo. "
