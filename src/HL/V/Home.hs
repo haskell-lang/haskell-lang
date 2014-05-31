231fbde18e9fbb3fdb3da1f2bf93abbaf4cbf2a8
Chris Done <chrisdone@gmail.com>
2014-05-31 03:29:49 +0200

    Factor out features section to separate module

--- src/HL/V/Home.hs
+++ src/HL/V/Home.hs
@@ -8,6 +8,7 @@ module HL.V.Home where
 import HL.V hiding (list)
 import HL.V.Code
 import HL.V.Template
+import HL.V.Home.Features
 
 -- | Home view.
 homeV :: FromSenza App
@@ -73,30 +74,11 @@ try url =
       (container
          (row
             (do span6 [] repl
-                span6 [id "guide"] rhs)))
+                span6 [id "guide"] (return ()))))
   where
     repl =
       do h2 [] "Try it"
          div [id "console"] (return ())
-    rhs =
-      do h2 [] "Got 5 minutes?"
-         p [] (do "Type "
-                  span [class_ "highlight"]
-                       "help"
-                  " to start an interactive tutorial.")
-         p [] "Or try typing these out and see what happens (click to insert):"
-         p [] (do haskellCode "23 * 36"
-                  " or "
-                  haskellCode "reverse \"hello\""
-                  " or "
-                  haskellCode "foldr (:) [] [1,2,3]"
-                  " or "
-                  haskellCode "do line <- getLine; putStrLn line"
-                  " or "
-                  haskellCode "readFile \"/welcome\"")
-         p [] (do a [href "https://hackage.haskell.org/package/pure-io-0.2.0/docs/PureIO.html#g:2"]
-                    "These"
-                  " IO actions are supported in this app.")
 
 -- | Community section.
 -- TOOD: Should contain a list of thumbnail videos. See mockup.
@@ -112,85 +94,6 @@ community url =
                              p [class_ "learn-more"]
                                (a [href (url CommunityR)]
                                   "Learn more")))))
-
--- | Features section explains what's notable about Haskell as a
--- language.
-features :: Senza
-features =
-  div [class_ "features"]
-      (container
-         (do h1 [] "Features"
-             row (do span6 [] purefunc
-                     span6 [] statically)
-             row (do span6 [] concurrent
-                     span6 [] inference)
-             row (do span6 [] lazy
-                     span6 [] packages)))
-
--- TODO: Features: tart up the wording.
---
--- Note: these below are me writing out the facts, for myself, rather
--- than putting in a way that newbies will understand. The intention
--- is to put *something* here and then rewrite the bits below to be
--- more "salesy" aand friendly to people who have no idea what's so
--- special about "IO" from any other form of programming, or what a
--- parallel GC is or unification.
-
-purefunc :: Senza
-purefunc =
-  do h2 [] "Purely functional"
-     p [] "Every function in Haskell is pure. They are functions in the mathematical sense. \
-          \Even side-effecting IO operations are but a description of what to do, produced \
-          \by pure code. There are no statements or instructions, only expressions. Which \
-          \cannot mutate variables, local or global, or access state like time or random \
-          \numbers."
-     p [] (a [] "View examples")
-
-statically :: Senza
-statically =
-  do h2 [] "Statically typed"
-     p [] "Every expression in Haskell has a type which is determined at compile time. \
-          \All the types composed together by function application have to match up. If \
-          \they don't, the program will be rejected by the compiler. Types become not \
-          \only a form of guarantee, but a language for expressing the construction \
-          \of programs."
-     p [] (a [] "View examples")
-
-concurrent :: Senza
-concurrent =
-  do h2 [] "Concurrent"
-     p [] "Haskell lends itself well to concurrent programming due to its explicit \
-          \handling of effects. Its flagship compiler, GHC, comes with a high-\
-          \performance parallel garbage collector and light-weight concurrency \
-          \library containing a number of useful concurrency primitives and \
-          \abstractions."
-     p [] (a [] "View examples")
-
-inference :: Senza
-inference =
-  do h2 [] "Type inference"
-     p [] "You don't have to explicitly write out every type in a Haskell program. \
-          \Types will be inferred by unifying every type bidirectionally. But you \
-          \can write out types, or ask the compiler to write them for you, for \
-          \handy documentation."
-     p [] (a [] "View examples")
-
-lazy :: Senza
-lazy =
-  do h2 [] "Lazy"
-     p [] "Functions don't evaluate their arguments. This means that programs \
-          \can compose together very well, with the ability to write control \
-          \constructs with normal functions, and, thanks also to the purity \
-          \of Haskell, to fuse chains of functions together for high \
-          \performance."
-     p [] (a [] "View examples")
-
-packages :: Senza
-packages =
-  do h2 [] "Packages"
-     p [] "Open source contribution to Haskell is very active with a wide range \
-          \of packages available on the public package servers."
-     p [] (a [] "View examples")
 
 -- | Events section.
 -- TODO: Take events section from Haskell News?
