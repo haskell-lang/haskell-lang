cc6531e8ac09c7fdf6ed64cceb8b1b731ab25ce2
Chris Done <chrisdone@gmail.com>
2014-06-08 13:16:57 +0200

    Update third party libs

--- src/HL/V/Downloads.hs
+++ src/HL/V/Downloads.hs
@@ -28,8 +28,7 @@ downloadsV =
                       (forM_ [minBound .. maxBound]
                              (\os -> li [] (a [href (url (DownloadsForR os))]
                                               (toHtml (toHuman os)))))
-                   h2 [] "Third party libraries"
-                   todo "Explanation of Hackage and Stackage here."))))
+                   thirdParty))))
 
 -- | OS-specific downloads view.
 downloadsForV :: OS -> FromSenza App
@@ -45,3 +44,59 @@ downloadsForV os =
                []
                (do h1 [] (toHtml ("Downloads for " <> toHuman os))
                    todo "Coming soon."))))
+
+thirdParty =
+  do h2 [] "Third party libraries"
+     p [] (do "In Haskell, packages are managed with the Cabal package \
+              \system built into GHC (and other compilers). "
+              "For more specific details, see "
+              (a [href "http://www.haskell.org/cabal/users-guide/"]
+                 "The Cabal User Guide")
+              ".")
+     hackage
+     stackage
+     github
+
+hackage =
+  do h3 [] "Hackage"
+     p [] (do "Hackage is a repository of packages to which anyone can freely \
+              \upload at any time. The packages are available immediately and \
+              \documentation will be generated and hosted there. It can be used by "
+              code [] "cabal install"
+              ".")
+     p [] (do "In your "
+              code [] ".cabal/config"
+              " file, there will by default be a line like the following:")
+     pre [] "remote-repo: hackage.haskell.org:http://hackage.haskell.org/packages/archive"
+     p [] "You can install a package by merely running: "
+     pre [] "$ cabal update \n\
+            \$ cabal install the-package"
+     p [] (a [href "http://hackage.haskell.org/packages/"] "Go to Hackage →")
+
+stackage =
+  do h3 [] "Stackage"
+     p [] "Stackage is a stable repository of snapshots of package sets in \
+          \which only packages which build and pass test together are bundled \
+          \together into a snapshot."
+     p [] (do "To use, in your "
+              code [] ".cabal/config"
+              ".cabal/config file, you specify something like the following:")
+     pre [] "remote-repo: stackage:http://www.stackage.org/stackage/<the-snapshot-hash>"
+     p [] "(Note: any other remote-repo line should be removed or commented out)."
+     p [] "After that, you can install a package by merely running: "
+     pre [] "$ cabal update \n\
+            \$ cabal install the-package"
+     p [] (a [href "http://hackage.haskell.org/packages/"] "Go to Stackage →")
+
+github =
+  do h3 [] "From source control repositories"
+     p [] "Installing from a source repository is also possible. For example, \
+          \to clone and install the network package from source, you would run:"
+     pre [] "$ git clone git@github.com:haskell/network.git\n\
+            \$ cabal install network/"
+     p [] "Or:"
+     pre [] "$ git clone git@github.com:haskell/network.git\n\
+            \$ cd network\n\
+            \$ cabal install"
+     p [] (a [href "https://github.com/trending?l=haskell&since=monthly"]
+             "Browse Github by Haskell repositories →")
