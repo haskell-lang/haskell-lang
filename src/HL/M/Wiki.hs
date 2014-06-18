a78f283ca2861baa8c78ec0cd3a7ad285281901b
Chris Done <chrisdone@gmail.com>
2014-06-18 15:54:17 +0200

    Handle pure exceptions from wiki parsing. Grr

--- src/HL/M/Wiki.hs
+++ src/HL/M/Wiki.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 -- | Model for wiki.
 
@@ -8,19 +9,18 @@ module HL.M.Wiki where
 
 import HL.C
 
+import Control.DeepSeq
 import Control.Exception.Lifted (catch)
+import Control.Spoon
 import Data.Conduit
-
 import Data.Maybe
 import Data.Monoid
 import Data.Text (unpack)
-
 import Network.HTTP.Conduit
 import Prelude hiding (readFile)
 import Text.Pandoc.Definition
 import Text.Pandoc.Options
 import Text.Pandoc.Readers.MediaWiki
-
 import Text.XML
 import Text.XML.Cursor
 
@@ -36,7 +36,12 @@ getWikiPage article =
                           (\(_::UnresolvedEntityException) -> return Nothing)
              case doc >>= parse of
                Nothing -> return (Left "Unable to parse XML from haskell.org.")
-               Just (title,pan) -> return (Right (title,pan)))
+               Just (title,pan) ->
+                 return
+                   (fromMaybe (Left ("Unable to parse XML from haskell.org! And the \
+                                     \parser gave an an impure exception! Can you \
+                                     \believe it?"))
+                              (showSpoon (Right (title,pan)))))
   where
     parse doc =
       do let cursor = fromDocument doc
@@ -63,3 +68,9 @@ getWikiPage article =
       element (name "title") >>=
       descendant >>=
       content
+
+-- | Make a spoon using the Show instance.
+showSpoon :: Show a => a -> Maybe a
+showSpoon a =
+  (fmap (const a)
+        (spoon (length (show a))))
