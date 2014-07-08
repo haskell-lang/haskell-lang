9637fb03bcbcdad7fbcae648871f6b0962e38b0b
Ricky Elrod <ricky@elrod.me>
2014-07-08 13:57:07 -0400

    Use the haskell.org google analytics code
    Summary: Signed-off-by: Ricky Elrod <ricky@elrod.me>
    
    Test Plan: None yet, cabal hates me.
    
    Reviewers: austin
    
    Reviewed By: austin
    
    Subscribers: relrod
    
    Differential Revision: https://phabricator.haskell.org/D47

--- src/HL/V/Template.hs
+++ src/HL/V/Template.hs
@@ -69,14 +69,16 @@ skeleton ptitle innerhead innerbody bodyender mroute url =
          bodyender mroute url
     -- TODO: pop this in a config file later.
     analytics =
-      script []
-             "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n\
-             \  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n\
-             \  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n\
-             \  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\
-             \\n\
-             \  ga('create', 'UA-51440536-1', 'haskell-lang.org');\n\
-             \  ga('send', 'pageview');"
+      script [] $ unlines
+             [ "var _gaq = _gaq || [];"
+             , "_gaq.push(['_setAccount', 'UA-15375175-1']);"
+             , "_gaq.push(['_trackPageview']);"
+             , "(function() {"
+             , "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;"
+             , "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';"
+             , "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);"
+             , "})();"
+             ]
 
 -- | Make a list of scripts.
 scripts :: (Route App -> AttributeValue) -> [Route Static] -> Senza
