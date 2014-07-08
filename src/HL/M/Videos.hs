054ab65bd56ddabe3366ab00c36b42e1a062399f
Ricky Elrod <ricky@elrod.me>
2014-07-08 17:01:41 -0400

    I missed a link
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/HL/M/Videos.hs
+++ src/HL/M/Videos.hs
@@ -19,7 +19,7 @@ getHomeVideos =
 -- concerned).
 vids :: [(Text,Text, Text)]
 vids =
-  [("Functional Reactive Programming for Musical User Interfaces by Paul Hudak","https://vimeo.com/96744621","http://i.vimeocdn.com/video/476988542_150x84.jpg")
+  [("Functional Reactive Programming for Musical User Interfaces by Paul Hudak","https://vimeo.com/96744621","https://i.vimeocdn.com/video/476988542_150x84.jpg")
   ,("Conquering Hadoop with Haskell and Ozgun Ataman","http://vimeo.com/90189610","https://i.vimeocdn.com/video/469235326_150x84.jpg")
   ,("Using Lenses to Structure State with Nathan Bouscal","http://vimeo.com/90184695","https://i.vimeocdn.com/video/469227196_150x84.jpg")
   ,("GHCJS: Bringing Haskell to the Browser, by Luite Stegeman","http://vimeo.com/80895330","https://i.vimeocdn.com/video/456929997_150x84.jpg")
