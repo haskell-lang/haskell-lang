58124dba74f6d58b20fbc27835685bcf4de7dac1
Chris Done <chrisdone@gmail.com>
2015-01-13 14:50:48 +0100

    Add getStaticDir
    This can be used to abstract over getDataFileName.

--- src/HL/Static.hs
+++ src/HL/Static.hs
@@ -1,8 +1,22 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 -- | Static files.
+
 module HL.Static where
 
+import Control.Monad.IO.Class
+import HL.Development
+
+import Paths_hl
 import Yesod.Static
 
 staticFiles "static/"
+
+-- | Get the directory for static files. In development returns the
+-- local copy, in production mode uses the Cabal data-files
+-- functionality.
+getStaticDir :: MonadIO m => m FilePath
+getStaticDir =
+  if development
+     then return "static/"
+     else liftIO (getDataFileName "static")
