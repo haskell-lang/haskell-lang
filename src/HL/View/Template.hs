dab601de2e5ca3241cea75c47c54e9a595cf1302
Ricky Elrod <ricky@elrod.me>
2015-02-15 01:17:16 -0500

    Put in the range, after -infrastructure discussion
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/HL/View/Template.hs
+++ src/HL/View/Template.hs
@@ -156,7 +156,7 @@ footer url r =
                       wikiLicense (Nothing :: Maybe Text)
                     _ -> hlCopy)))
   where hlCopy =
-          do span_ [class_ "item"] "\169 2015 haskell.org"
+          do span_ [class_ "item"] "\169 2014-2015 haskell.org"
              span_ [class_ "item footer-contribute"]
                    (do "Got changes to contribute? "
                        a_ [href_ "https://github.com/haskell-infra/hl"] "Fork on Github")
