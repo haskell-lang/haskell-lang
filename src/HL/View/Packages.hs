75fde33cca281864d143077c208d1f1aa2f2dbec
Michael Snoyman <michael@snoyman.com>
2016-05-04 11:04:31 +0300

    Packages have self-links, correctly heading level

--- src/HL/View/Packages.hs
+++ src/HL/View/Packages.hs
@@ -24,7 +24,7 @@ packagesV pi = template [] "Packages"
                 mapM_ quickLink (fmap packageName (foldMap commonChoices (piCommons pi))))
           h2_ [id_ "_fundamentals"] "Fundamentals"
           toHtml (piFundamentalsIntro pi)
-          row_ (mapM_ package (piFundamentals pi))
+          row_ (mapM_ (package False) (piFundamentals pi))
           h2_ [id_ "_commons"] "Commons"
           toHtml (piCommonsIntro pi)
           mapM_ common (piCommons pi)
@@ -33,9 +33,11 @@ packagesV pi = template [] "Packages"
 quickLink :: Text -> Html ()
 quickLink name = li_ (a_ [href_ ("#" <> name)] (toHtml name))
 
-package :: Package -> Html ()
-package f = (span4_ [class_ "col-md-4"] (do
-  h3_ [id_ (packageName f)] (toHtml (packageName f))
+package :: Bool -> Package -> Html ()
+package isCommon f = (span4_ [class_ "col-md-4"] (do
+  let heading_ = if isCommon then h4_ else h3_
+  heading_ [id_ (packageName f)]
+    (a_ [href_ ("#" <> packageName f)] (toHtml (packageName f)))
   toHtml (packageDesc f)
   ul_ [class_ "quicklinks"]
     (mapM_ link
@@ -50,4 +52,4 @@ common c = do
     let ident = "_common_" <> commonSlug c
     h3_ [id_ ident] (a_ [href_ ("#" <> ident)] (toHtml (commonTitle c)))
     toHtml (commonDesc c)
-    row_ (mapM_ package (commonChoices c))
+    row_ (mapM_ (package True) (commonChoices c))
