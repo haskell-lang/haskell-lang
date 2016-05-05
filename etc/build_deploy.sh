4192c43d5109fe19e6808eb390362707f8e1df7a
Emanuel Borsboom <manny@fpcomplete.com>
2016-05-05 05:03:41 -0700

    etc/build_deploy.sh: fix build argument passing

--- etc/build_deploy.sh
+++ etc/build_deploy.sh
@@ -4,6 +4,6 @@ set -xe
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 ENV="$1"; shift
 stack --docker --docker-auto-pull build "$@"
-etc/docker/build.sh --docker --no-build "$@"
+etc/docker/build.sh --docker --no-build
 etc/docker/push.sh "$ENV"
 etc/kubernetes/deploy_rc.sh "$ENV"
