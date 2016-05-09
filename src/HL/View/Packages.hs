2ea3732a9751985b1a1d556301b5372d6aabe4e7
Chris Done <chrisdone@gmail.com>
2016-05-09 19:53:27 +0200

    Add common tasks

--- src/HL/View/Packages.hs
+++ src/HL/View/Packages.hs
@@ -31,32 +31,33 @@ content pi =
      toHtml (piIntro pi)
      h2_ [id_ "_cores"] "Core"
      toHtml (piFundamentalsIntro pi)
-     mapM_ (row_ . mapM_ (package False))
+     mapM_ (row_ . mapM_ (span3_ [class_ "col-md-3"] . package False))
            (chunksOf 4 (toList (piFundamentals pi)))
-     -- h2_ [id_ "_commons"] "Common"
-     -- toHtml (piCommonsIntro pi)
-     -- mapM_ common (piCommons pi)
+     h2_ [id_ "_commons"] "Common"
+     toHtml (piCommonsIntro pi)
+     mapM_ (row_ . mapM_ common)
+           (chunksOf 2 (toList (piCommons pi)))
 
 package :: Bool -> Package -> Html ()
 package isCommon f =
-  (span4_ [class_ "col-md-3"]
-          (a_ [class_ "package-big-link"
-              ,href_ ("https://www.stackage.org/package/" <> packageName f)]
-              (do let heading_ =
-                        if isCommon
-                           then h4_
-                           else h3_
-                  heading_ [id_ (packageName f)]
-                           (toHtml (packageName f))
-                  span_ [class_ "pkg-desc"]
-                        (toHtml (packageDesc f)))))
+  a_ [class_ "package-big-link"
+     ,href_ ("https://www.stackage.org/package/" <> packageName f)]
+     (do let heading_ =
+               if isCommon
+                  then h4_
+                  else h3_
+         heading_ [id_ (packageName f)]
+                  (toHtml (packageName f))
+         span_ [class_ "pkg-desc"]
+               (toHtml (packageDesc f)))
 
 common :: Common -> Html ()
 common c =
-  do let ident = "_common_" <> commonSlug c
-     h3_ [id_ ident]
-         (a_ [href_ ("#" <> ident)]
-             (toHtml (commonTitle c)))
-     toHtml (commonDesc c)
-     row_ (mapM_ (package True)
-                 (commonChoices c))
+  span6_ [class_ "col-md-6"]
+         (do do let ident = "_common_" <> commonSlug c
+                h3_ [id_ ident]
+                    (a_ [href_ ("#" <> ident)]
+                        (toHtml (commonTitle c)))
+                toHtml (commonDesc c)
+                row_ (mapM_ (span6_ [class_ "col-md-6"] . package True)
+                            (commonChoices c)))
