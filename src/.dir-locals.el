75ffcb95b2e797f86fe349a21d382607b6124688
Chris Done <chrisdone@gmail.com>
2016-05-13 17:47:27 +0200

    Remove .dir-locals.el file

--- src/.dir-locals.el
+++
@@ -1,8 +0,0 @@
-((haskell-mode
-  . ((haskell-process-type . ghci)
-     (haskell-process-path-ghci . "stack")
-     (haskell-process-use-ghci . t)
-     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng" "--no-build" "--no-load"))))
- (haskell-cabal-mode
-  . ((haskell-process-path-ghci . "stack")
-     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng")))))
