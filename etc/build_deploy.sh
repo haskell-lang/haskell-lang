95673e9939b7941f1b0ed657d1c62a537998e3d4
Emanuel Borsboom <manny@fpcomplete.com>
2016-07-14 13:44:15 -0700

    etc: remove now-superfluous files

--- etc/build_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# See description at https://github.com/fpco/devops-helpers#wrappers
-set -xe
-cd "$(dirname "${BASH_SOURCE[0]}")/.."
-ENV="$1"; shift
-stack --docker --docker-auto-pull build "$@"
-etc/docker/build.sh --docker --no-build
-etc/docker/push.sh "$ENV"
-etc/kubernetes/deploy_rc.sh "$ENV"
