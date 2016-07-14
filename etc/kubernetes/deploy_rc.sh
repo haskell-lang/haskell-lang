f7bea624f9cc267131a86d1f8a33a752e19c6d35
Emanuel Borsboom <manny@fpcomplete.com>
2016-07-14 13:44:15 -0700

    etc: remove now-superfluous files

--- etc/kubernetes/deploy_rc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# See description at https://github.com/fpco/devops-helpers#wrappers
-set -xe
-exec "$(dirname ${BASH_SOURCE[0]})/../common/devops-helpers/kubernetes/deploy_rc_helper.sh" \
-     --app "haskell-lang" \
-     --repo fpco/haskell-lang \
-     --specdir "$(dirname "${BASH_SOURCE[0]}")" \
-     "$@"
