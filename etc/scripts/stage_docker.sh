da8da8969e679dcda8bd9e0e8ec6282730adefd7
Emanuel Borsboom <manny@fpcomplete.com>
2016-07-14 21:06:30 -0700

    etc: update docker artifact path

--- etc/scripts/stage_docker.sh
+++ etc/scripts/stage_docker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
-stack install --local-bin-path=etc/docker "$@"
-cp -r config static themes etc/docker/
+mkdir -p etc/docker/_artifacts
+stack install --local-bin-path=etc/docker/_artifacts "$@"
+cp -r config static themes etc/docker/_artifacts/
